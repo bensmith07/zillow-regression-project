5df3776426b3a1f928714e2b61f71c264cf8035c
Ben Smith <ben.f.smith07@gmail.com>
2022-03-24 16:49:51 -0500

    removed tax_amount from SQL query. adjusted query syntax, resulting in accurate number of observations acquired.

--- acquire.py
+++ acquire.py
@@ -3,8 +3,6 @@ import pandas as pd
 import env
 
 def zillow_2017_data():
-
-#TODO: adjust SQL query to acquire only properties with a TRANSACTION in 2017
     
     filename = 'zillow.csv'
     
@@ -15,22 +13,17 @@ def zillow_2017_data():
     url = env.get_db_url('zillow')
     sql = '''
             SELECT bedroomcnt, 
-                   bathroomcnt, 
-                   calculatedfinishedsquarefeet, 
-                   taxvaluedollarcnt, 
-                   yearbuilt, 
-                   taxamount,
-                   fips 
+                    bathroomcnt, 
+                    calculatedfinishedsquarefeet, 
+                    taxvaluedollarcnt, 
+                    yearbuilt, 
+                    fips 
               FROM properties_2017
-                LEFT JOIN propertylandusetype USING (propertylandusetypeid)
+                JOIN propertylandusetype USING (propertylandusetypeid)
+                JOIN predictions_2017 USING(parcelid)
               WHERE propertylandusedesc IN ("Single Family Residential", 
                                             "Inferred Single Family Residential")
-                  AND parcelid IN (
-                                   SELECT properties_2017.parcelid
-                                     FROM properties_2017
-                                       JOIN predictions_2017 USING(parcelid)
-                                     WHERE transactiondate LIKE "2017%%"
-                                  );
+                AND transactiondate LIKE "2017%%";
             '''
     
     print('No local file exists\nReading from SQL database...')
